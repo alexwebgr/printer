d60385a06861fa40f36b4397d3bf428e930e2808
alexwebgr <websurfer992@gmail.com>
2021-11-01 17:32:02 +0200

    couple more tests and simplecov

--- spec/printer_spec.rb
+++ spec/printer_spec.rb
@@ -1,24 +1,53 @@
 # frozen_string_literal: true
-
+require 'spec_helper'
 require './printer'
 
-describe Printer do
+RSpec.describe Printer do
+  include_context("file_helper")
+
   describe '#print' do
-    let(:output) { Printer.print(10) }
-    it 'returns the list' do
-      expect(output).to eq(<<~EOS
-        1
-        2
-        People
-        4
-        Power
-        People
-        7
-        8
-        People
-        Power
-      EOS
-      )
+    describe 'when the param is missing' do
+      let(:expected_output) { file_fixture('100_items.txt').read }
+
+      it 'returns the list with the default number of items' do
+        expect(described_class.print).to eq(expected_output)
+      end
+    end
+
+    describe 'when times is 0' do
+      let(:times) { 0 }
+      let(:expected_output) { file_fixture('empty.txt').read }
+
+      it 'returns the empty message' do
+        expect(described_class.print(times)).to eq(expected_output)
+      end
+    end
+
+    describe 'when times is 10' do
+      let(:times) { 10 }
+      let(:expected_output) { file_fixture('10_items.txt').read }
+
+      it 'returns the list' do
+        expect(described_class.print(times)).to eq(expected_output)
+      end
+    end
+
+    describe 'when times is 100' do
+      let(:times) { 100 }
+      let(:expected_output) { file_fixture('100_items.txt').read }
+
+      it 'returns the list' do
+        expect(described_class.print(times)).to eq(expected_output)
+      end
+    end
+
+    describe 'when times is 110' do
+      let(:times) { 110 }
+      let(:expected_output) { file_fixture('110_items.txt').read }
+
+      it 'returns the list' do
+        expect(described_class.print(times)).to eq(expected_output)
+      end
     end
   end
 end
